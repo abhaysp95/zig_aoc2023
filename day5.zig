f260c93abd2708dc480a7afbcfd715dc3d3894f3
abhaysp95 <abhaysp95@protonmail.com>
2024-04-07 21:40:52 +0530

    feat:  day2 code done
      answer is wrong for some reason, will debug later

--- day5.zig
+++ day5.zig
@@ -12,18 +12,100 @@ const Mapper = struct {
     dest: usize,
     src: usize,
     range: usize,
+
+    fn fits(self: Mapper, value: usize) bool {
+        return if (value >= self.src and value <= self.src + self.range) true else false;
+    }
 };
 
 fn find_num(needle: usize, haystack: []const Mapper) usize {
     var ans = needle;
     for (haystack) |hay| {
-        if (needle >= hay.src and needle <= hay.src + hay.range) {
+        if (hay.fits(needle)) {
             ans = hay.dest + (needle - hay.src);
             break;
         }
     }
 
     return ans;
+}
+
+fn parseMapper(mappings: [][]const Mapper, value: usize) usize {
+    const location = find_num(value, mappings[6]);
+    const humidity = find_num(location, mappings[5]);
+    const temperature = find_num(humidity, mappings[4]);
+    const light = find_num(temperature, mappings[3]);
+    const water = find_num(light, mappings[2]);
+    const fert = find_num(water, mappings[1]);
+    const soil = find_num(fert, mappings[0]);
+
+    return soil;
+}
+
+fn part2(allocator: mem.Allocator, seeds: []const usize, mappings: [][]const Mapper) !void {
+    var start: usize = 1;
+    var reverse_mappings = try allocator.alloc([]Mapper, mappings.len);
+    for (mappings, 0..) |mapping, i| {
+        reverse_mappings[i] = try allocator.alloc(Mapper, mapping.len);
+        for (mapping, 0..) |map, j| {
+            reverse_mappings[i][j].src = map.dest;
+            reverse_mappings[i][j].dest = map.src;
+            reverse_mappings[i][j].range = map.range;
+        }
+    }
+
+    for (reverse_mappings) |mapping| {
+        for (mapping) |map| {
+            dprint("src: {}, dest: {}, range: {}\n", .{ map.src, map.dest, map.range });
+        }
+        dprint("\n", .{});
+    }
+
+    outerLoop: while (true) {
+        var got_seed = parseMapper(reverse_mappings, start);
+        var idx: usize = 0;
+        while (idx <= seeds.len / 2) {
+            if (seeds[idx] <= got_seed and seeds[idx] + seeds[idx + 1] > got_seed) {
+                break :outerLoop;
+            }
+            idx += 2;
+        }
+
+        if (start % 10_000_000 == 0) {
+            dprint("start: {} | got_seed: {}\n", .{ start, got_seed });
+        }
+
+        start += 1;
+    }
+
+    dprint("got_seed: {}\n", .{start});
+
+    // while (seed_idx < seeds.len) : (seed_idx += 2) {
+    //     var start: usize = seeds[seed_idx];
+    //     var end: usize = seeds[seed_idx] + seeds[seed_idx + 1];
+    //     while (start < end) : (start += 1) {
+    //         if (map.get(start) != null) {
+    //             continue;
+    //         }
+    //         const soil = find_num(start, mappings[0]);
+    //         const fert = find_num(soil, mappings[1]);
+    //         const water = find_num(fert, mappings[2]);
+    //         const light = find_num(water, mappings[3]);
+    //         const temperature = find_num(light, mappings[4]);
+    //         const humidity = find_num(temperature, mappings[5]);
+    //         const location = find_num(humidity, mappings[6]);
+    //         try map.put(start, location);
+    //         // min_loc = @min(min_loc, location);
+    //         // dprint("seed: {}, soil: {}, fert: {}, water: {}, light: {}, temp: {}, hum: {}, loc: {}, min_loc: {}\n", .{ start, soil, fert, water, light, temperature, humidity, location, min_loc });
+    //     }
+    // }
+    //
+    // var miter = map.iterator();
+    // while (miter.next()) |iter| {
+    //     min_loc = @min(min_loc, iter.value_ptr.*);
+    // }
+
+    // dprint("min location: {}\n", .{min_loc});
 }
 
 fn part1(seeds: []const usize, mappings: [][]const Mapper) !void {
@@ -112,6 +194,8 @@ pub fn main() !void {
         }
         dprint("\n", .{});
     }
+    dprint("\n---------------------------\n", .{});
 
-    try part1(seeds, mappings);
+    // try part1(seeds, mappings);
+    try part2(allocator, seeds, mappings);
 }
